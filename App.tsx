c9803c5f7bb2607b97d30549d3392d9a1ae057b3
신승훈 <zlzlzlmo@daum.net>
2022-06-14 22:29:09 +0900

    홈 화면 레이아웃 적용 (SafeAreaView, Platform, StatusBar Api 사용)

--- App.tsx
+++ App.tsx
@@ -1,17 +1,32 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./components/screens/HomeScreen";
-import LoginScreen from "./components/screens/LoginScreen";
-
-const Stack = createNativeStackNavigator();
+import {
+  StyleSheet,
+  View,
+  Text,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
+import { colors } from "./src/utils/colors";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+    </SafeAreaView>
   );
 }
+
+// flex 1을 줌으로써 full screen을 쓴다는 설정이된다
+//SafeAreaView는 휴대폰 기기의 경계선에 알맞게 렌더링을 해준다
+// 현재 IOS 디바이스에만 적용인된다. (11버전 이상)
+// 때문에 안드로이드는 Platform과 StatusBar Api를 사용해서 상태표시줄 만큼 padding top을 해줘야한다.
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+  text: {
+    color: colors.white,
+  },
+});
--- styles/variables.ts
+++
@@ -1,3 +0,0 @@
-export const colors = {
-  mainColor: "#175CEE",
-};
